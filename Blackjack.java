95c0b876c6bd8a5a17850ff3781c88f148342bd2
RedactedFromPrint <32402605+RedactedFromPrint@users.noreply.github.com>
2017-10-02 18:04:56 -0700

    Home Edits 10/2/17
    Sumbitted this version to Classroom

--- Blackjack.java
+++ Blackjack.java
@@ -12,7 +12,7 @@ public class Blackjack{
        Hand cpuHand = new Hand();
        while (cpuHand.getTotal() < 15){
            cpuHand.hit();
-        }
+       }
        System.out.println("You start with a total of " + playerHand.getTotal());
        boolean stop = false;
        while (stop == false){
@@ -32,19 +32,22 @@ public class Blackjack{
                 else {
                     System.out.println("You tied at a total of " + playerHand.getTotal());
                 }
-            stop = true;
+                stop = true;
             }
             else {
                 System.out.println("Your new total is " + playerHand.getTotal());
             }
         }
         else if (choice.equals("s")){
-            if (playerHand.getTotal() > cpuHand.getTotal()){
+            if (cpuHand.getTotal() > 21){
+                System.out.println("The CPU busted! You win with a total of " + playerHand.getTotal());
+            }
+            else if (playerHand.getTotal() > cpuHand.getTotal()){
                 System.out.println("You win! Your hand of " + playerHand.getTotal() + " beat the CPU's total of " + cpuHand.getTotal());
             }
-            else if (playerHand.getTotal() < cpuHand.getTotal()){
+            else if (playerHand.getTotal() < cpuHand.getTotal() && cpuHand.getTotal() < 22){
                 System.out.println("You lose. Your hand of " + playerHand.getTotal() + " lost to the CPU's total of " + cpuHand.getTotal());
-            }
+            } 
             else {
                 System.out.println("You tied at a total of " + playerHand.getTotal());
             }
@@ -54,6 +57,5 @@ public class Blackjack{
             System.out.println("Invalid input");
         }
        }
-       
    }
 }
